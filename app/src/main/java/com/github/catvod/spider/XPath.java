8dff3c347d11bdc9bbd573435be21541214ca452
唐三 <89683694+Tangsan99999@users.noreply.github.com>
2021-11-25 00:30:48 +0800

    Update XPath.java

--- app/src/main/java/com/github/catvod/spider/XPath.java
+++ app/src/main/java/com/github/catvod/spider/XPath.java
@@ -21,6 +21,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
+import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class XPath extends Spider {
 
